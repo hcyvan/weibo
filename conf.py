2c18f577fa52b91732351d05365784166e8f9cf7
Navy Cheng <navycheng2020@gmail.com>
2017-10-11 09:20:01 +0800

    add

--- conf.py
+++ conf.py
@@ -1,15 +1,13 @@
 class Config:
     ACCOUNT = 'nipponpaint01@sina.com',
     HEADERS = {
-    'x-requested-with': "XMLHttpRequest",
+    'upgrade-insecure-requests': "1",
     'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-    'content-type': "application/x-www-form-urlencoded",
-    'accept': "*/*",
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
     'dnt': "1",
-    'referer': "http://weibo.com/u/6375348295/home?wvr=5",
-    'accept-encoding': "gzip, deflate",
+    'accept-encoding': "gzip, deflate, br",
     'accept-language': "zh-CN,zh;q=0.8,en;q=0.6",
-    'cookie': "TC-Page-G0=4e714161a27175839f5a8e7411c8b98c; login_sid_t=ef418c059e09a2acca5e5998fceaf875; TC-Ugrow-G0=5e22903358df63c5e3fd2c757419b456; TC-V5-G0=914ba011d20e5b7f25fe12574c186eda; _s_tentry=-; Apache=9363813775934.143.1501120965634; SINAGLOBAL=9363813775934.143.1501120965634; ULV=1501120966627:1:1:1:9363813775934.143.1501120965634:; YF-V5-G0=24e0459613d3bbdec61239bc81c89e13; YF-Page-G0=c81c3ead2c8078295a6f198a334a5e82; YF-Ugrow-G0=56862bac2f6bf97368b95873bc687eef; SCF=At7gTE9mZY24pjXXLYMcbHT7MhqT5xfPzyQ-UW9c1ng7Z7a8iH-N3iLh0aJwIvkwaIL9IhmkcUJGOfNFR-Jdtno.; SUB=_2A2502MnHDeRhGeBN7FcS9CbOwjmIHXVXr7wPrDV8PUNbmtAKLRXSkW9VoV_TL-qR9EEvSXrRSnrnM2phtw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFT4IKzjgpGHOliDQ_.8F265JpX5K2hUgL.Foq0S0-0ShnE1K-2dJLoIp7LxKML1KBLBKnLxKqL1hnLBoMce0Mfe0BReo.f; SUHB=079T2dc1Y_Ej0s; ALF=1508242455; SSOLoginState=1507637655; un=13873824730; wvr=6; wb_cusLike_6375348295=Y; WBtopGlobal_register_version=1844f177002b1566; UOR=,,www.baidu.com",
+    'cookie': "TC-Page-G0=4e714161a27175839f5a8e7411c8b98c; login_sid_t=ef418c059e09a2acca5e5998fceaf875; TC-Ugrow-G0=5e22903358df63c5e3fd2c757419b456; TC-V5-G0=914ba011d20e5b7f25fe12574c186eda; _s_tentry=-; Apache=9363813775934.143.1501120965634; SINAGLOBAL=9363813775934.143.1501120965634; ULV=1501120966627:1:1:1:9363813775934.143.1501120965634:; YF-V5-G0=24e0459613d3bbdec61239bc81c89e13; YF-Page-G0=c81c3ead2c8078295a6f198a334a5e82; YF-Ugrow-G0=56862bac2f6bf97368b95873bc687eef; WBStorage=569d22359a08e178|undefined; SSOLoginState=1507651675; SCF=Am_HuG2SGDn2umlUm1ExylGtMdqLESLGyZh7KuCGRB0jx7Plp0Auhvj-MXu2VuK_IVH-UTZHcxav8ou-7_AHyMo.; SUB=_2A2502IAMDeRhGeNH7VMQ9SfLyjSIHXVXr_bErDV8PUNbmtBeLRXgkW8Nn7kLuYc1nG1FDyN0vBm27h6pUg..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhJlDs.b23M_9oEye.fZneo5JpX5K2hUgL.Fo-4So2pSK.NeKn2dJLoI0qLxKqLBonL1KBLxK-LBo5L12qLxKBLB.BLBoeLxKqLBK-LBK5LxKqL122LBKBLxK-L1-eLBKet; SUHB=0gHiPHnMCOt16O; ALF=1539187674; un=15676213514; httpsupgrade_ab=SSL; wvr=6; wb_cusLike_5961159718=N",
     'cache-control': "no-cache",
-    'postman-token': "a933ccba-53eb-7c25-00d4-5e7fea1c8aa0"
+    'postman-token': "41322897-f14a-fdc7-0998-2503c1ea06f2"
     }
